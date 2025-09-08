b8ea669ef4dd61c6b596a4fe30ef4b869a5d7268
yash-k11 <khandagleyash11@gmail.com>
2025-09-08 18:15:20 +0530

    Fix base URL for Netlify deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: '', // This will fix the issue by generating relative paths
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
